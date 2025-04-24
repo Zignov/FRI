e78eb93713b769b66092202b418f1b98529b76cb
Zignov <ziga.novak28@gmail.com>
2025-04-24 11:02:05 +0200

    DN07

---
+++ FirstYear/SecondSemester/java/vaje/bank/aplikacija/Upravljanje.java
@@ -0,0 +1,56 @@
+package vaje.bank.aplikacija;
+
+import vaje.bank.banka.Banka;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Upravljanje {
+
+    // Iz podane datoteke prebere podatke o računih in ustvari račune v podani banki.
+    private static void dodajRacune(String vir, Banka banka) throws FileNotFoundException {
+        Scanner vhod = new Scanner(new File(vir));
+        while (vhod.hasNextLine()) {
+            String[] podatki = vhod.nextLine().split(";");
+            if (podatki[0].equalsIgnoreCase("tekoci")) { // ustvari tekoči račun
+                banka.dodajTekociRacun(podatki[1], Double.parseDouble(podatki[2]));
+            } else { // ustvari varčevalni račun
+                banka.dodajVarcevalniRacun(podatki[1], Double.parseDouble(podatki[2]));
+            }
+            banka.polog(podatki[1], Double.parseDouble(podatki[3]));
+        }
+        vhod.close();
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        // ustvarimo novo banko
+        Banka bankaFRI = new Banka();
+
+        // v banki naredimo račune z določenimi stanji, vse podatke preberemo iz datoteke
+        dodajRacune("viri/racuni.txt", bankaFRI);
+
+        // TODO: izpiši vse račune
+        bankaFRI.izpisiRacune();
+
+        // TODO: izpiši vse tekoče račune
+        bankaFRI.izpisiRacune(false);
+
+        String stevilkaRacuna = "SI56 1234 4321 1234 126"; // tekoči z limitom 100 €
+        // String stevilkaRacuna = "SI56 7823 4563 8346 123"; // varčevalni z 0,1% obrestmi
+
+
+        // TODO: položi znesek na račun in preveri novo stanje z izpisom računov
+        bankaFRI.polog(stevilkaRacuna, 50000);
+
+
+        // TODO: dvigni znesek z računa in preveri novo stanje z izpisom računov
+        bankaFRI.dvig(stevilkaRacuna, 80);
+        bankaFRI.izpisiRacune();
+
+
+        // TODO: preveri še ostale metode banke
+
+    }
+}
---
+++ FirstYear/SecondSemester/java/vaje/bank/banka/Racun.java
@@ -0,0 +1,127 @@
+package vaje.bank.banka;
+
+public abstract class Racun {
+    private double stanje; // stanje na računu
+    private final String stevilka; // številka računa
+
+    /**
+     * Konstruktor: ustvari račun s podano številko računa in začetnim stanjem 0,00 EUR.
+     *
+     * @param stevilka - številka računa
+     */
+    Racun(String stevilka) {
+        this.stanje = 0.0;
+        this.stevilka = stevilka;
+    }
+
+    /**
+     * Položi znesek "znesek" na račun.
+     *
+     * @param znesek - položen znesek, pozitivno število
+     * @return true, če je polog uspešno izveden, sicer false (v primeru nepozitivnega pologa)
+     */
+    public boolean polog(double znesek) {
+        if(znesek<=0){
+            return false;
+        }
+        stanje += znesek;
+        return true;
+    }
+
+    /**
+     * Dvigne znesek "znesek" z računa. Stanje računa je lahko tudi negativno.
+     *
+     * @param znesek - dvignjen znesek, pozitivno število
+     * @return true, če je dvig uspešno izveden, sicer false (v primeru nepozitivnega zneska)
+     */
+    public boolean dvig(double znesek) {
+        if(znesek<=0){
+            return false;
+        }
+        stanje -= znesek;
+        return true;
+    }
+
+    /**
+     * Vrne trenutno stanje na računu (getter).
+     *
+     * @return stanje na računu
+     */
+    public double getStanje() {
+        return stanje;
+    }
+
+    /**
+     * Vrne številko računa (getter).
+     *
+     * @return številka računa
+     */
+    public String getStevilka() {
+        return stevilka;
+    }
+
+    /**
+     * Abstraktna metoda, ki vrne opis računa glede na njegov tip.
+     *
+     * @return niz z opisom računa
+     */
+    abstract String opisRacuna();
+
+    /**
+     * Pripravi niz, ki opisuje račun in njegovo trenutno stanje.
+     * Niz je naslednje oblike: "Račun <stevilka> (<opis>): <stanje> EUR",
+     * kjer je <stevilka> številka računa, <stanje> je stanje na računu, <opis> pa je opis tipa računa.
+     *
+     * @return niz, ki opisuje račun in stanje na njem
+     */
+    public String toString() {
+        String rez = String.format("Račun <%s> (<%s>): <%.2f> EUR", getStevilka(), opisRacuna(), getStanje());
+        return rez;
+    }
+
+}
+
+class VarcevalniRacun extends Racun{
+    private double obresti;
+    private double stanje = super.getStanje();
+
+    VarcevalniRacun(String stevilka, double obresti){
+        super(stevilka);
+        this.obresti = obresti;
+    }
+
+
+    public void dodajObresti(){
+         stanje *= obresti;
+    }
+
+    public String opisRacuna(){
+        String rez = String.format("(varčevalni, obrestna mera: %.3f): %.2f EUR", obresti, stanje);
+        return rez;
+    }
+}
+
+class TekociRacun extends Racun{
+    private double limit;
+    double stanje = super.getStanje();
+
+    TekociRacun(String stevilka, double limit){
+        super(stevilka);
+        this.limit = limit;
+    }
+
+    @Override
+    public boolean dvig(double znesek){
+        if(znesek<=limit){
+            super.dvig(znesek);
+            return true;
+        }
+        return false;
+    }
+
+    public String opisRacuna(){
+        String rez = String.format("(tekoči, limit: %.2f EUR): %.2f EUR", limit, stanje);
+        return rez;
+    }
+}
+
