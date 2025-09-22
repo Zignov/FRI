193beef2a1a2ddaf02c5c7472114129535bc1e08
Zignov <ziga.novak28@gmail.com>
2025-09-22 09:19:33 +0200

    Refactor Naloga11.java to improve code structure and readability

--- FirstYear/SecondSemester/java/izpiti/naloga11.java
+++ FirstYear/SecondSemester/java/izpiti/naloga11.java
@@ -1,49 +1,43 @@
 //package izpiti;
 //
-//import java.io.File;
-//import java.util.ArrayList;
-//import java.util.Collection;
-//import java.util.HashMap;
-//import java.util.Scanner;
 //
-//public class naloga11 {
+//import java.io.File;
+//import java.util.*;
+//
+//public class Naloga11 {
 //
 //    public static String stetje(String v){
+//        ArrayList<String> besede = new ArrayList<>();
 //        try{
 //            File vhod = new File(v);
-//            Scanner sc = new Scanner (vhod);
+//            Scanner sc = new Scanner(vhod);
 //
-//            ArrayList<String> besede = new ArrayList<>();
-//            HashMap<String, Integer> stevec = new HashMap<>();
 //
-//            while (sc.hasNextLine()){
-//                String[] zac = sc.nextLine().split("/0");
-//                for(int i = 0; i<zac.length; i++){
-//                    besede.add(zac[i]);
-//                }
+//
+//
+//            while(sc.hasNextLine()){
+//                String vrsta = sc.nextLine();
+//                String[] skupaj = vrsta.split("\0");
+//
+//                besede.addAll(Arrays.asList(skupaj));
 //            }
-//
-//
-//            for(int i = 0; i<besede.size(); i++){
-//                if(stevec.containsKey(besede.get(i))){
-//                    stevec.put(besede.get(i), stevec.get(besede.get(i)) + 1);
-//                }
-//                else{
-//                    stevec.put(besede.get(i), 1);
-//                }
-//            }
-//
-//
-//            System.out.println(stevec.values());
 //        }
-//        catch(Exception e){
+//        catch (Exception e){
+//        }
 //
+//        Dictionary<String, Integer> stetje = new Hashtable<>();
+//
+//        for (int i = 0; i<besede.size(); i++){
+//            stetje.put(b)
 //        }
 //    }
 //
-//    public static void main(String []args){
+//
+//
+//
+//
+//
+//    public static void main(String args[]){
 //
 //    }
-//
-//
 //}
